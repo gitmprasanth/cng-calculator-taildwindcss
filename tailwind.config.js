dd5f1d90d0f646cabeea606c83dba281e12eacc6
PRASANTH M <151944542+gitmprasanth@users.noreply.github.com>
2025-01-02 16:20:20 +0530

    design-ui

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+    "./index.html",
+    "./assets/css/main.css",
+  ],
+
+  theme: {
+    extend: {
+      fontFamily:{
+        'roboto':'"Roboto", serif'
+      }
+    },
+  },
+  plugins: [],
+}
