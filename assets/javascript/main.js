0d379fa9c1d7e8155703fd5b0b43269d6c7bf242
PRASANTH M <151944542+gitmprasanth@users.noreply.github.com>
2025-01-08 19:37:21 +0530

    calculating fuel cost

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,3 +7,32 @@ if(firstName&&lastName){
     guestTxt.innerText=firstName+" "+lastName
 }
 
+const tripdistanceEl=document.getElementById("trip-distance")
+const fuelEffEl=document.querySelector("#fuel-efficiency")
+const fuelEl=document.querySelector("#fuel-cost")
+const calculateBtnEl=document.querySelector("#calculateBtn")
+
+calculateBtnEl.addEventListener("click",(event)=>{
+    event.preventDefault();
+    if(tripdistanceEl.value === ""){
+        alert("Enter the Trip distance")
+    }
+    else{
+        const distanceKm=tripdistanceEl.value
+        const fuelEfficiency=fuelEffEl.value
+        const fuelcost=fuelEl.value
+        
+        const result=(distanceKm/fuelEfficiency)*fuelcost
+        const finalmsg="Your Fuel cost will be around ₹ "+result
+       console.log(finalmsg) 
+       alert(finalmsg)       
+    }
+
+
+})
+
+
+// const result=500/24;
+
+//Milege :20.83
+
